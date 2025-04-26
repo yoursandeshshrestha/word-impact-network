b80b730737e234059d599c674b5034c3e607d898
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-04-27 01:34:23 +0530

    (feat): integrate application routes into main router for application management

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,16 @@
 import express, { Router } from 'express';
 import adminRoutes from './admin.routes';
 import studentRoutes from './student.routes';
-
+import applicationRoutes from './application.routes';
 const router: Router = express.Router();
 
+// admin routes
 router.use('/admin', adminRoutes);
 
 // student routes
 router.use('/student', studentRoutes);
+
+// application routes
+router.use('/applications', applicationRoutes);
 
 export default router;
