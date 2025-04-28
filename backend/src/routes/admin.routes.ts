cd8f4f96f3bb8761332654b654af83e0ec92a7c1
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-04-28 23:16:38 +0530

    feat: add admin profile route and integrate authentication middleware

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -1,10 +1,16 @@
 import express, { Router } from 'express';
-import { registerAdmin, loginAdminController } from '../controllers/admin.controller';
+import {
+  registerAdmin,
+  loginAdminController,
+  getAdminProfile,
+} from '../controllers/admin.controller';
 import { validateAdminLogin, validateAdminRegister } from '../validations/admin.validation';
+import { authenticate } from '@/middlewares/auth.middleware';
 
 const router: Router = express.Router();
 
 router.post('/create-admin', validateAdminRegister, registerAdmin);
 router.post('/login-admin', validateAdminLogin, loginAdminController);
+router.get('/profile', authenticate, getAdminProfile);
 
 export default router;
