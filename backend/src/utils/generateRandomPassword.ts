423625e1f9658cc6b5ad69ca48f99ca4a05c782f
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-04-28 23:54:27 +0530

    feat: add utility function to generate random passwords for enhanced security features

---
+++ backend/src/utils/generateRandomPassword.ts
@@ -0,0 +1,13 @@
+function generateRandomPassword(length = 10): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  return password;
+}
+
+export default generateRandomPassword;
