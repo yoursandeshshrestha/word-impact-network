4ceb91c3a20daccc8a98f62c53082c5c2f4f2612
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-04 21:18:01 +0530

    feat: add SocketEvents enum for websocket event handling

---
+++ backend/src/websocket/types.ts
@@ -0,0 +1,8 @@
+export enum SocketEvents {
+  CONNECTION = 'connection',
+  CONNECTED = 'connected',
+  NEW_MESSAGE = 'new_message',
+  MESSAGE_READ = 'message_read',
+  DISCONNECT = 'disconnect',
+  ANNOUNCEMENT = 'announcement',
+}
