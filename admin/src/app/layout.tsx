e989c1f1344ded6854f35b7bac53185d2db5c626
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-05 03:08:32 +0530

    fix: restore children rendering in RootLayout component for proper layout structure

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
         <Toaster position="top-right" richColors />
+        {children}
       </body>
     </html>
   );
