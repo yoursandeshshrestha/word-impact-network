cb4a7e589c6a17edad554e5875c537dbe85b7b36
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-08 01:13:00 +0530

    feat: enhance ProtectedLayout component with user information retrieval and notification support

--- admin/src/app/(pages)/(protected)/layout.tsx
+++ admin/src/app/(pages)/(protected)/layout.tsx
@@ -1,22 +1,40 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "@/src/components/layout/Sidebar";
 import { getUserInfo } from "@/src/utils/auth";
 import Topbar from "@/src/components/layout/Topbar";
+import { Toaster } from "sonner";
+
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const userInfo = getUserInfo();
+  const [userName, setUserName] = useState("Admin User");
+  const [userEmail, setUserEmail] = useState("admin@example.com");
+
+  useEffect(() => {
+    const userInfo = getUserInfo();
+    console.log("User info from cookies:", userInfo);
+
+    if (userInfo) {
+      setUserName(userInfo.fullName);
+      setUserEmail(userInfo.email);
+    }
+  }, []);
 
   return (
     <div className="flex h-screen bg-gray-50">
-      <Sidebar userName={userInfo?.fullName} userEmail={userInfo?.email} />
-      <div className="w-full">
-        <Topbar userName={userInfo?.fullName} userRole="Administrator" />
-        <main className="flex-1 md:ml-64 p-4 overflow-y-auto">{children}</main>
+      <div className="fixed inset-y-0 left-0 w-64">
+        <Sidebar userName={userName} userEmail={userEmail} />
       </div>
+      <div className="flex-1 ml-64">
+        <Topbar userName={userName} userRole="Administrator" />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50">
+          {children}
+        </main>
+      </div>
+      <Toaster position="top-right" richColors />
     </div>
   );
 }
