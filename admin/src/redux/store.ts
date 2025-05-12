d114f22a4e13eff02da4867b45971ae429a7d5a6
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-13 00:23:41 +0530

    feat: enhance Redux store configuration with persistence for students and adminProfile reducers, and add courses and loading reducers

--- admin/src/redux/store.ts
+++ admin/src/redux/store.ts
@@ -2,17 +2,40 @@ import { configureStore } from "@reduxjs/toolkit";
 import applicationsReducer from "./features/applicationsSlice";
 import studentsReducer from "./features/studentsSlice";
 import adminProfileReducer from "./features/adminProfileSlice";
+import coursesReducer from "./features/coursesSlice";
+import loadingReducer from "./features/loadingSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["students", "adminProfile"], // Only persist these reducers
+};
+
+const persistedStudentsReducer = persistReducer(persistConfig, studentsReducer);
+const persistedAdminProfileReducer = persistReducer(
+  persistConfig,
+  adminProfileReducer
+);
+
 export const store = configureStore({
   reducer: {
     applications: applicationsReducer,
-    students: studentsReducer,
-    adminProfile: adminProfileReducer,
+    students: persistedStudentsReducer,
+    adminProfile: persistedAdminProfileReducer,
+    courses: coursesReducer,
+    loading: loadingReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
