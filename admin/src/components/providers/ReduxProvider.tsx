64ac1b84bc112163233c56c1046fdf3fd3a0c28b
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-13 00:22:59 +0530

    feat: integrate Redux Persist for state persistence in ReduxProvider component

--- admin/src/components/providers/ReduxProvider.tsx
+++ admin/src/components/providers/ReduxProvider.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "@/src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/redux/store";
 
 export default function ReduxProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
