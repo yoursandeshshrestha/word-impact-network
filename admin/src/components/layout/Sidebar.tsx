96b76ae93d1d8d2370eeac164918309b25dd16b8
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-13 00:22:48 +0530

    refactor: update import path for logout utility in Sidebar component

--- admin/src/components/layout/Sidebar.tsx
+++ admin/src/components/layout/Sidebar.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { logout } from "@/src/utils/auth";
+import { logout } from "@/utils/auth";
 import { useRouter } from "next/navigation";
 import {
   Home,
