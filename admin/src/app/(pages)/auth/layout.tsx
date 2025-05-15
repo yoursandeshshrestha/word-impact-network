823db8a85ec5f8df327203c69aca5fe9c5e778b1
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-16 01:01:11 +0530

    refactor: comment out unused imports in AuthLayout component

--- admin/src/app/(pages)/auth/layout.tsx
+++ admin/src/app/(pages)/auth/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { getAuthToken } from "@/src/utils/auth";
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { getAuthToken } from "@/src/utils/auth";
 
 export default function AuthLayout({
   children,
