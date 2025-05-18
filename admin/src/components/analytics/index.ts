161d251ee574b3aa66eae905b66e77a8d0575543
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-19 00:35:17 +0530

    feat: export analytics components for enhanced data visualization, including EnrollmentTrendsChart, CourseCompletionChart, VideoEngagementChart, GeographicDistributionChart, and MetricCard

---
+++ admin/src/components/analytics/index.ts
@@ -0,0 +1,6 @@
+// Export all analytics components
+export { default as EnrollmentTrendsChart } from "./EnrollmentTrendsChart";
+export { default as CourseCompletionChart } from "./CourseCompletionChart";
+export { default as VideoEngagementChart } from "./VideoEngagementChart";
+export { default as GeographicDistributionChart } from "./GeographicDistributionChart";
+export { default as MetricCard } from "./MetricCard";
