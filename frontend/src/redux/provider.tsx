4a2aacc55260752a2936829cf358ee79e909625c
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-21 21:38:59 +0530

    feat: implement ReduxProvider component to integrate Redux store with React application

---
+++ frontend/src/redux/provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { ReactNode } from "react";
+
+interface ReduxProviderProps {
+  children: ReactNode;
+}
+
+export function ReduxProvider({ children }: ReduxProviderProps) {
+  return <Provider store={store}>{children}</Provider>;
+}
