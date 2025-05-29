fd3eb8ece363e8b9811fda21fd5e6905c8a7913c
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-05-29 22:54:51 +0530

    style: adjust main content padding in DashboardLayout for improved layout consistency

--- frontend/src/app/(no-layout)/layout.tsx
+++ frontend/src/app/(no-layout)/layout.tsx
@@ -37,7 +37,7 @@ export default function DashboardLayout({
         />
 
         {/* Main Content */}
-        <main className="flex-1 p-4 sm:p-6 lg:p-8">
+        <main className="flex-1 ">
           <div>{children}</div>
         </main>
       </div>
