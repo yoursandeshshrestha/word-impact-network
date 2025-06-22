493bc8def3aca095a502137db2ed4499ebbd6a0f
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-06-22 20:03:27 +0530

    feat: enhance middleware to manage authentication redirects for protected and auth pages

--- admin/src/middleware.ts
+++ admin/src/middleware.ts
@@ -3,15 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
+  // Get auth token from cookies
   const authToken = request.cookies.get("authToken")?.value;
 
+  // If user is on auth pages and has a token, redirect to dashboard
   if (path.startsWith("/auth/") && authToken) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
+  // If user is on protected pages and doesn't have a token, redirect to login
   if (!path.startsWith("/auth/") && !authToken) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
+
   return NextResponse.next();
 }
 
