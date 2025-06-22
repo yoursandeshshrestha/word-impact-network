a8e8c3248b63347cdc4277ca1f31e0c812fb85ae
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-06-22 20:03:02 +0530

    feat: add OurLeadershipTeamPage component to render the leadership team section

---
+++ frontend/src/app/(main)/(static)/our-leadership-team/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import OurLeadershipTeam from "@/components/Leadership/OurLeadershipTeam";
+
+export default function OurLeadershipTeamPage() {
+  return <OurLeadershipTeam />;
+}
