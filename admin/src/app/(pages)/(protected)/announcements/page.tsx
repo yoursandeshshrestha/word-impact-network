914eeffa3e636249234077185dfbdb35e23becf8
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-07-05 12:58:02 +0530

    feat: add AnnouncementsPage component to display announcements list, enhancing user engagement and information accessibility

---
+++ admin/src/app/(pages)/(protected)/announcements/page.tsx
@@ -0,0 +1,9 @@
+import AnnouncementsList from "@/components/announcements/AnnouncementsList";
+
+export default function AnnouncementsPage() {
+  return (
+    <div className="p-6">
+      <AnnouncementsList />
+    </div>
+  );
+}
