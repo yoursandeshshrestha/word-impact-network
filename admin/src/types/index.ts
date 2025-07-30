1c0a15720ac07b66cbd2ab0f247686b1fd188675
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-07-31 04:01:46 +0530

    feat: add SocketEvents enum for managing WebSocket event types

--- admin/src/types/index.ts
+++ admin/src/types/index.ts
@@ -66,3 +66,20 @@ export type CoursePageParams = {
  *   // Rest of component...
  * }
  */
+
+/**
+ * Socket Events
+ */
+export enum SocketEvents {
+  CONNECTION = 'connection',
+  CONNECTED = 'connected',
+  NEW_MESSAGE = 'new_message',
+  MESSAGE_READ = 'message_read',
+  DISCONNECT = 'disconnect',
+  ANNOUNCEMENT = 'announcement',
+  NOTIFICATION = 'notification',
+  NOTIFICATION_READ = 'notification_read',
+  NOTIFICATION_READ_ALL = 'notification_read_all',
+  VIDEO_STATUS_UPDATE = 'video_status_update',
+  VIDEO_PROCESSING_PROGRESS = 'video_processing_progress',
+}
